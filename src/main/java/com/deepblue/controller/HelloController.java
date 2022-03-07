09a532047338c26cc782320d9b6c3eae60aceaef
yangjianwei <2017585616@qq.com>
2022-03-07 23:40:50 +0800

    first commit

--- src/main/java/com/deepblue/controller/HelloController.java
+++ src/main/java/com/deepblue/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.deepblue.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @RequestMapping("/sayHello")
     public String sayHello() {
         return "Hello Jenkins";
     }
